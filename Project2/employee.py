4421aef3fd876a91b899e932924c733085f6a228
Ilyas Rufai <rufaiilyas@gmail.com>
2022-09-27 23:49:13 +0100

    Update employee.py

--- Project2/employee.py
+++ Project2/employee.py
@@ -9,7 +9,25 @@ class Employee:
         self.position = position
         self.salary = salary
         Employee.count+=1
-        def displayCount(self):
-            print("There are %d employee" % Employee.count)
-        def displayDetails(self):
-            print("Name:", self.name, "position:", self.position, "salary:", self.salary)
+    def displayCount(self):
+            print("There are %d employees" % Employee.count)
+    def displayDetails(self):
+            print("Name:", self.name, ", Position:", self.position, ", Salary:", self.salary)
+
+#
+emp = Employee("Ilyas Rufai", "DevOps Engineer", 10000000)
+emp1 = Employee("Ayo", "HR", 300000)
+emp2 = Employee("Aisruffy", "manager", 900000)
+emp3 = Employee("Alex", "programmer", 500000)
+emp4 = Employee("AbdulRahmon", "Engineer", 45000)
+
+#Count of employees
+emp4.displayCount()
+
+#
+print("**Information about the Employee**")
+emp.displayDetails()
+emp1.displayDetails()
+emp2.displayDetails()
+emp3.displayDetails()
+emp4.displayDetails()
