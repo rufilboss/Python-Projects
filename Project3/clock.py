29c72f34316c3050f1fb8e09f309af966dd82a51
Ilyas Rufai <rufaiilyas@gmail.com>
2022-09-28 21:37:50 +0100

    Update clock.py

--- Project3/clock.py
+++ Project3/clock.py
@@ -1,3 +1,11 @@
+from curses import window
 from time import strftime
 from tkinter import Label, Tk
 
+
+#Designing the GUI for the digital clock
+window = Tk()
+window.title("")
+window.geometry("200 * 80")
+window.configure(bg = "black")
+window.resizable(False, False)
