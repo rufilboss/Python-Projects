e37c41fba0a2888de618afa72c7dca9c0c540faa
Ilyas Rufai <rufaiilyas@gmail.com>
2022-09-29 23:28:48 +0100

    Update Gguess.py

--- Project4/gguess.py
+++ Project4/gguess.py
@@ -5,8 +5,21 @@ def guessingGame(guessLimit, number):
 
     try:
         while guessLimit > 0:
-            guess = int(input("Enter your input..."))
-            
+            guess = int(input("What is your guess?"))
+            guessLimit -= 1
+
+        if randomNumber == guess:
+            print('Congrats, you got it right!')
+            # break
+        elif guess > number:
+            print('Your guess is out of range')
+            print(f'You have {guessLimit} guess(es) left!')
+        else:
+            print('Sorry, you are wrong!')
+            print(f'You have {guessLimit} guess(es) left!')
+
+        print('Game Over!')
+        print(f'The random number was {randomNumber}')
 
     except ValueError:
         print("Only number is allowed")
