15e54e1a4329e1000de709008454a930fc898ba4
Ilyas Rufai <rufaiilyas@gmail.com>
2024-09-27 15:39:45 +0100

    update

--- Digital Clock/clock.py
+++ Digital Clock/clock.py
@@ -1,27 +1,19 @@
-from curses import window
 from time import strftime
-from tkinter import Label, Tk, mainloop
+from tkinter import Label, Tk
 
-
-#Designing the GUI for the digital clock
 window = Tk()
 window.title("Digital Clock")
-window.geometry("200 * 80")
-window.configure(bg = "black")
+window.geometry("200x80")
+window.configure(bg="black")
 window.resizable(False, False)
 
-#Labeling
-clock_label = Label(window, bg= "white", fg= "green", font= ("Times", 30, "bold"), relief= "flat")
-clock_label.place(x = 20, y = 20)
+clock_label = Label(window, bg="black", fg="green", font=("Times", 30, "bold"), relief="flat")
+clock_label.place(x=20, y=20)
 
-
-#Writing the func
 def update_label():
-    current_time = strftime("%H: %M: %S")
-    clock_label.configure(text= current_time)
-    clock_label.after(80, update_label)
+    current_time = strftime("%H:%M:%S") 
+    clock_label.configure(text=current_time)
+    clock_label.after(1000, update_label)
 
-
-#Calling the func
 update_label()
-window,mainloop()
+window.mainloop()
